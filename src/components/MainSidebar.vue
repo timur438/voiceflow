4d8cb060caf5b36864902874b2569f8e13584db8
andrelebedev5500 <andrelebedev5500@proton.me>
2025-01-23 23:22:18 +0300

    Created sidebar component

---
+++ src/components/MainSidebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <aside class="sidebar">
+    <div class="sidebar-content">
+      <div class="logo" @click="goToHome">
+        <div class="logo-box"></div>
+        <button class="logo-button">Whisper</button>
+      </div>
+      <nav class="menu">
+        <ul>
+          <li><a href="#"><img src="@/assets/img/category.svg" alt="Мои встречи" />Мои встречи</a></li>
+          <li><a href="#"><img src="@/assets/img/setting.svg" alt="Настройки и интеграции" />Настройки и интеграции</a></li>
+          <li><a href="#"><img src="@/assets/img/logout.svg" alt="Выйти" />Выйти</a></li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  name: 'MainSidebar',
+  setup() {
+    const router = useRouter();
+    const goToHome = () => {
+      router.push({ name: 'MainView' });
+    };
+    return { goToHome };
+  }
+});
+</script>
+
+<style scoped src="@/assets/scss/MainSidebar.scss"></style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="main-container">
-    <aside class="sidebar">
-      <div class="sidebar-content">
-        <div class="logo">
-          <div class="logo-box"></div>
-          <button class="logo-button" @click="goToHome">Whisper</button>
-        </div>
-        <nav class="menu">
-          <ul>
-            <li><a href="#"><img src="@/assets/img/category.svg" alt="Мои встречи" />Мои встречи</a></li>
-            <li><a href="#"><img src="@/assets/img/setting.svg" alt="Настройки и интеграции" />Настройки и интеграции</a></li>
-            <li><a href="#"><img src="@/assets/img/logout.svg" alt="Выйти" />Выйти</a></li>
-          </ul>
-        </nav>
-      </div>
-    </aside>
+    <MainSidebar />
     <div class="content">
       <h1>Добро пожаловать на главную страницу</h1>
       <!-- Add more content here -->
@@ -25,9 +11,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
+import MainSidebar from '@/components/MainSidebar.vue';
 
 export default defineComponent({
   name: 'MainView',
+  components: {
+    MainSidebar
+  },
   setup() {
     const router = useRouter();
     const goToHome = () => {
